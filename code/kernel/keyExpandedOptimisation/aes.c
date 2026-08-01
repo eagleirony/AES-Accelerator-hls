2fbdcaaded68af3d9daa821196f68bc3a2b867e6
Xavier Herbert <z5478413@ad.unsw.edu.au>
2026-08-01 15:37:33 +1000

    added statment labels to the loops and compressed two stages into one to try and minimise risk of not making shift rows into wires

--- code/kernel/keyExpandedOptimisation/aes.c
+++ code/kernel/keyExpandedOptimisation/aes.c
@@ -27,7 +27,7 @@
 // the upcoming round It also moves the pointer round_key to the start of the
 // 128 byte section needed
 static void get_round_key(uint8_t * key, uint16_t round) {
-    for (uint16_t current_size = round * AES_BLOCK_SIZE;
+generate_key:    for (uint16_t current_size = round * AES_BLOCK_SIZE;
          current_size < round * AES_BLOCK_SIZE + AES_BLOCK_SIZE;
          current_size += WORD_SIZE) {
 #pragma HLS unroll
@@ -40,7 +40,7 @@ static void get_round_key(uint8_t * key, uint16_t round) {
 #pragma HLS unroll
             temp_word[i] = key[(current_size - WORD_SIZE + i) % AES_KEY_SIZE];
         }
-        if (current_size % (uint16_t)AES_KEY_SIZE == 0) {
+key_size_sbox:        if (current_size % (uint16_t)AES_KEY_SIZE == 0) {
             uint8_t rcon_iteration = current_size / AES_KEY_SIZE;
             uint8_t temp = sbox[temp_word[0]];
             temp_word[0] = sbox[temp_word[1]] ^ rcon[rcon_iteration];
@@ -49,14 +49,14 @@ static void get_round_key(uint8_t * key, uint16_t round) {
             temp_word[3] = temp;
         }
 #if AES_VERSION == AES_256
-        if ((current_size % AES_KEY_SIZE) == AES_BLOCK_SIZE) {
+key_block_sbox:        if ((current_size % AES_KEY_SIZE) == AES_BLOCK_SIZE) {
             for (uint16_t i = 0; i < WORD_SIZE; i++) {
 #pragma HLS unroll
                 temp_word[i] = sbox[temp_word[i]];
             }
         }
 #endif
-        for (uint16_t i = 0; i < WORD_SIZE; i++) {
+key_update:        for (uint16_t i = 0; i < WORD_SIZE; i++) {
 #pragma HLS unroll
             key[(current_size + i) % AES_KEY_SIZE] =
                 key[(current_size + i) % AES_KEY_SIZE] ^ temp_word[i];
@@ -64,46 +64,42 @@ static void get_round_key(uint8_t * key, uint16_t round) {
     }
 }
 
-static void sub_bytes(aes_state_t * state) {
-    for (int r = 0; r < AES_STATE_DIM; ++r) {
-#pragma HLS unroll
-        for (int c = 0; c < AES_STATE_DIM; ++c) {
-#pragma HLS unroll
-            (*state)[r][c] = sbox[(*state)[r][c]];
-        }
-    }
-}
+static void shift_rows_and_sub_bytes(aes_state_t * state) {
+    //Row 0: 0-byte left shift
+    (*state)[0][0] = sbox[(*state)[0][0]];
+    (*state)[0][1] = sbox[(*state)[0][1]];
+    (*state)[0][2] = sbox[(*state)[0][2]];
+    (*state)[0][3] = sbox[(*state)[0][3]];
 
-static void shift_rows(aes_state_t * state) {
     uint8_t temp;
     // Row 1: 1-byte left shift
-    temp = (*state)[1][0];
-    (*state)[1][0] = (*state)[1][1];
-    (*state)[1][1] = (*state)[1][2];
-    (*state)[1][2] = (*state)[1][3];
+    temp = sbox[(*state)[1][0]];
+    (*state)[1][0] = sbox[(*state)[1][1]];
+    (*state)[1][1] = sbox[(*state)[1][2]];
+    (*state)[1][2] = sbox[(*state)[1][3]];
     (*state)[1][3] = temp;
 
     // Row 2: 2-byte left shift
-    temp = (*state)[2][0];
-    (*state)[2][0] = (*state)[2][2];
-    (*state)[2][2] = temp;
-    temp = (*state)[2][1];
-    (*state)[2][1] = (*state)[2][3];
-    (*state)[2][3] = temp;
+    uint8_t temp2 = sbox[(*state)[2][0]];
+    (*state)[2][0] = sbox[(*state)[2][2]];
+    (*state)[2][2] = temp2;
+    uint8_t temp3 = sbox[(*state)[2][1]];
+    (*state)[2][1] = sbox[(*state)[2][3]];
+    (*state)[2][3] = temp3;
 
     // Row 3: 3-byte left shift
-    temp = (*state)[3][0];
-    (*state)[3][0] = (*state)[3][3];
-    (*state)[3][3] = (*state)[3][2];
-    (*state)[3][2] = (*state)[3][1];
-    (*state)[3][1] = temp;
+    uint8_t temp4 = sbox[(*state)[3][0]];
+    (*state)[3][0] = sbox[(*state)[3][3]];
+    (*state)[3][3] = sbox[(*state)[3][2]];
+    (*state)[3][2] = sbox[(*state)[3][1]];
+    (*state)[3][1] = temp4;
 }
 
 static void mix_columns(aes_state_t * state) {
     uint8_t t[AES_STATE_DIM];
-    for (int c = 0; c < AES_STATE_DIM; ++c) {
+mix_columns_outer:    for (int c = 0; c < AES_STATE_DIM; ++c) {
 #pragma HLS unroll
-        for (int r = 0; r < AES_STATE_DIM; ++r) {
+mix_columns_outer_inner:         for (int r = 0; r < AES_STATE_DIM; ++r) {
 #pragma HLS unroll
             t[r] = (*state)[r][c];
         }
@@ -120,9 +116,9 @@ static void mix_columns(aes_state_t * state) {
 
 static void add_round_key(aes_state_t * state, const uint8_t * round_key,
                           const uint16_t round) {
-    for (int c = 0; c < AES_STATE_DIM; ++c) {
+key_xor:    for (int c = 0; c < AES_STATE_DIM; ++c) {
 #pragma HLS unroll
-        for (int r = 0; r < AES_STATE_DIM; ++r) {
+key_xor_inner:        for (int r = 0; r < AES_STATE_DIM; ++r) {
 #pragma HLS unroll
             (*state)[r][c] ^=
                 round_key[(round * AES_BLOCK_SIZE + (c * AES_STATE_DIM + r)) %
@@ -133,42 +129,39 @@ static void add_round_key(aes_state_t * state, const uint8_t * round_key,
 
 static void cipher_encrypt_block(aes_state_t * state, uint8_t * key) {
     add_round_key(state, key, 0);
-    for (uint16_t round = 1; round < AES_ROUNDS; round++) {
-#pragma HLS unroll
+round_loop:    for (uint16_t round = 1; round < AES_ROUNDS; round++) {
+#pragma HLS pipeline
         get_round_key(key, round);
-        sub_bytes(state);
-        shift_rows(state);
+        shift_rows_and_sub_bytes(state);
         mix_columns(state);
         add_round_key(state, key, round);
     }
     get_round_key(key, AES_ROUNDS);
-    sub_bytes(state);
-    shift_rows(state);
+    shift_rows_and_sub_bytes(state);
     add_round_key(state, key, AES_ROUNDS);
 }
 
 void aes_encrypt(const uint8_t * plaintext, uint8_t * ciphertext,
                  const uint8_t * key) {
     aes_state_t state;
-    for (int r = 0; r < AES_STATE_DIM; ++r) {
+mutable_plaintext:    for (int r = 0; r < AES_STATE_DIM; ++r) {
 #pragma HLS unroll
-        for (int c = 0; c < AES_STATE_DIM; ++c) {
+mutable_plaintext_inner:        for (int c = 0; c < AES_STATE_DIM; ++c) {
 #pragma HLS unroll
             state[r][c] = plaintext[r + AES_STATE_DIM * c];
         }
     }
 
     uint8_t changing_key[AES_KEY_SIZE];
-    for (int b = 0; b < AES_KEY_SIZE; ++b) {
+mutable_key:    for (int b = 0; b < AES_KEY_SIZE; ++b) {
 #pragma HLS unroll
         changing_key[b] = key[b];
     }
-
     cipher_encrypt_block(&state, changing_key);
 
-    for (int r = 0; r < AES_STATE_DIM; ++r) {
+copy_out_ciphertext:    for (int r = 0; r < AES_STATE_DIM; ++r) {
 #pragma HLS unroll
-        for (int c = 0; c < AES_STATE_DIM; ++c) {
+copy_out_ciphertext_inner:        for (int c = 0; c < AES_STATE_DIM; ++c) {
 #pragma HLS unroll
             ciphertext[r + AES_STATE_DIM * c] = state[r][c];
         }
