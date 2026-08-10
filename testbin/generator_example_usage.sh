7fbab98415ea79f9d29e1b9488619d72074325f0
daniel <dan.ben@hotmail.com.au>
2026-08-10 14:36:23 +1000

    added cypher text golden output for complete works of shakespeare

--- testbin/generator_example_usage.sh
+++ testbin/generator_example_usage.sh
@@ -1,2 +1,2 @@
 source /myenv/bin/activate
-python3 generator.py -p 192_plaintext.bin -k 192_key.bin -o test_out.bin
+python3 generator.py -p completeWorksOfShakespeare.txt -k 192_key.bin -o test_out.bin
